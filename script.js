e8bf961778fc08069e9e4c6f667aa601578c330c
Antoine Chagnon Michaud <achagnonmichaud@cvm.qc.ca>
2021-01-12 14:51:58 -0500

    01, 2, 3

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-document.getElementById("paragraphe").style.color = 'red'
+/* window.addEventListener('load', () => { n'est pas nessaire si on a defer  */
+// Juss pour Amandine
+// const changerCouleur = () => {
+//     document.getElementById("paragraphe").style.color = 'red'
+// }
+// window.addEventListener('load', changerCouleur)
+
+
+
+window.addEventListener('load', () => {
+    document.getElementById("paragraphe").style.color = 'red'
+})
